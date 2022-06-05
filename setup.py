c2579a32545f2dd5945d312732530a9322f50b3c
0noder4 <bartosz.kuklewski@tm1.edu.pl>
2022-06-05 22:39:37 +0200

    library base

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='pyBlindDriver',
+    packages=find_packages(include=['pyBlindDriver']),
+    version='0.0.1',
+    description='My first Python library',
+    author='0noder4',
+    license='MIT',
+)
